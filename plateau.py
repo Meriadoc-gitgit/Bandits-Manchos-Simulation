281eec3ab923bf5e020b30c589a3da0a67ce46c4
Thuy Duong <vuhoang.thduong@gmail.com>
2023-09-27 09:53:33 +0200

    update code for the second part

--- plateau.py
+++ plateau.py
@@ -1,7 +1,7 @@
 import numpy as np 
+import random
 from player import *
 from src import *
-import random
 
 class Plateau : 
 
@@ -21,6 +21,16 @@ class Plateau :
     print("Current state of the game")
     print("=============================")
     print(self.board)
+
+  #===============================
+  # Créer un duplicata du Plateau
+  def duplicata(self) : 
+    plt = Plateau(self.width, self.length, self.player1, self.player2)
+    for i in range(plt.length-1) : 
+      for j in range(self.width-1) : 
+        plt.board[i][j] = self.board[i][j]
+    return plt
+
 
 
   # Function for class manipulation 
@@ -74,35 +84,44 @@ class Plateau :
         turn = self.play(x,player1)
       elif turn==-1 : 
         turn = self.play(x,player2)
-      
 
-# NEW
-# Partie 2 - Monte Carlo
-def Monte_Carlo_play(self, etat, joueur) : 
-  N = self.width*self.length
-  recompenses = 0 #moyenne des victoires par action 
-  L = np.zeros(self.length)
 
-  while i<N : 
-    action = random.randint(0,self.length)
-
-    # Simuler 1 joueur virtuel
-    if etat == 1 : joueur_virtuel = PLayer(-1)
-    else : joueur_virtuel = Player(1)
+  def mc_strategy(self, etat, joueur) : 
+    N = self.width*self.length
+    recompenses = 0 #moyenne des victoires par action 
+    L = np.zeros(self.length)
+    i = 0
 
     # Simuler un autre Plateau virtuel
-    plateau = Plateau(self)
+    plateau = self.duplicata()
 
-    while not self.is_finished() :
+    while i<N : 
+      action = random.randint(0,self.length-1)
+      #print("action: ",action)
+
+      # Simuler 1 joueur virtuel
+      if etat == 1 : joueur_virtuel = Player(-1)
+      else : joueur_virtuel = Player(1)
+      
+      #while not self.is_finished() :
       plateau.play(action, joueur)
       plateau.run(joueur, joueur_virtuel)
-    
-    winner = plateau.has_won()
-    if winner==etat :  
-      for i in range(plateau.length) : 
-        for j in range(plateau.width) : 
-          if plateau.board[i][j] == etat : 
-            recompenses+=1
-      L[action] = recompenses
-    recompenses = 0
-  return np.argmin(L)
+
+      winner = plateau.has_won()
+      #print("winner: ",winner)
+      
+      if winner==etat :  
+        for j in range(plateau.length-1) : 
+          for k in range(plateau.width-1) : 
+            if plateau.board[j][k] == etat : 
+              recompenses+=1
+        
+        if L[action] > recompenses : 
+          L[action] = recompenses
+        elif L[action] == 0 :
+          L[action] = recompenses
+      recompenses = 0
+      self.reset()
+      i+=1
+    print(L)
+    return np.argmin(L)
--- player.py
+++ player.py
@@ -1,7 +1,3 @@
 class Player : 
   def __init__(self, value) : 
     self.etiquette = value
-    self.coordinate = set()
-  
-  def addCoordinate(self, coord) : 
-    self.coordinate.add(coord)
