f096bcbe48d246c98b05f6da9d1e6d3438a102d7
Thuy Duong <vuhoang.thduong@gmail.com>
2023-10-11 22:31:29 +0200

    update project

--- projet2-3i005/projet.py
+++ projet2-3i005/projet.py
@@ -6,25 +6,18 @@
 #####
 
 
-
-
 # Import necessary libraries
 import pandas as pd
 import numpy as np
 from scipy import stats
 
-class projet : 
 
-  def __init__(self) : 
-    # Initialiser la classe projet
-    pass
-
-  # Question 1.1 : calcul de probabilite a priori
-  @staticmethod
-  def getPrior(df) : 
-    return {
-      "estimation" : len([i for i in df["target"] if i==1])/len(df),
-      "min5pourcent" : stats.t.interval(0.95, df=len(df)-1, loc=np.mean(df["target"]), scale=np.std(df["target"], ddof=1) / np.sqrt(len(df)-1))[0],
-      "max5pourcent" : stats.t.interval(0.95, df=len(df)-1, loc=np.mean(df["target"]), scale=np.std(df["target"], ddof=1) / np.sqrt(len(df)-1))[1]
-    }
+# Question 1.1 : calcul de probabilite a priori
+  
+def getPrior(df) : 
+  return {
+    "estimation" : len([i for i in df["target"] if i==1])/len(df),
+    "min5pourcent" : stats.t.interval(0.95, df=len(df)-1, loc=np.mean(df["target"]), scale=np.std(df["target"], ddof=1) / np.sqrt(len(df)-1))[0],
+    "max5pourcent" : stats.t.interval(0.95, df=len(df)-1, loc=np.mean(df["target"]), scale=np.std(df["target"], ddof=1) / np.sqrt(len(df)-1))[1]
+  }
 
